8c11520e3b6ce483a5d6764a6a37a87ce5871a28
zsp <15396226187@163.com>
2021-04-24 14:29:28 +0800

    完成了整个请假流程的测试

--- rest-test/src/test/java/com/zsp/RestTestApplicationTests.java
+++ rest-test/src/test/java/com/zsp/RestTestApplicationTests.java
@@ -2,14 +2,17 @@ package com.zsp;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.task.Task;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
 class RestTestApplicationTests {
-//    使用activity工作流先生成数据库
+//    使用activity工作流先生成数据库表：23张
     @Test
     void testCreateTable(){
 //        使用xml生成activity工作流
@@ -47,7 +50,43 @@ class RestTestApplicationTests {
 @Test
     public void testStartProcessInstance(){
         ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        //act_re_procdef表里的id，如果没生成，去看上一步，真是哔了狗
         processEngine.getRuntimeService()
-                .startProcessInstanceById("askRest");
+                .startProcessInstanceById("myProcess_1:1:4");
     }
+
+//3、请假人发出请假申请
+@Test
+    public void testAsk(){
+//        获取activity7的引擎
+    ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+//    查看act_ru_task表，然后把id给上，发起请假申请
+    processEngine.getTaskService().complete("7505");
+
+    }
+//    4、老师查看请假要求
+@Test
+public void queryTask(){
+//        获得流引擎
+    ProcessEngine engine = ProcessEngines.getDefaultProcessEngine();
+//    创造搜索，下面的teacher是我们在创建bpmn的时候写的assignee值
+    List<Task> student = engine.getTaskService().createTaskQuery()
+            .taskAssignee("teacher")
+            .list();
+    for (Task task : student) {
+        System.out.println(task);
+    }
+}
+
+//5、班主任审批
+@Test
+public void testFinishTask_manager(){
+//        获取流引擎
+    ProcessEngine engine = ProcessEngines.getDefaultProcessEngine();
+//    这里的id是上面查询生成的请假申请id
+    engine.getTaskService()
+            .complete("10002");
+}
+
+
 }
